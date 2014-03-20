c108149bde92ada955c1c245697bdb8f4f55c77b
Alistair <Alistair@10.97.194.154>
2014-03-20 19:47:36 +0000

    slight change to ensure the program doesn't try to create a new index everytime a new user creates an account

--- Coursework2/src/com/coursework2/alistair/models/Playlists.java
+++ Coursework2/src/com/coursework2/alistair/models/Playlists.java
@@ -22,8 +22,8 @@ session.execute("CREATE KEYSPACE IF NOT EXISTS UserDetails WITH replication "
          + "= {'class':'SimpleStrategy', 'replication_factor':1};");
 session.execute("CREATE TABLE IF NOT EXISTS UserDetails.Users (UserName text, Password text, PRIMARY KEY (UserName));");
 session.execute("CREATE TABLE IF NOT EXISTS UserDetails.UserPlaylists (Username text, PlaylistName text, PlaylistPos int, TrackTitle text, Artist text, Album text, Trackid text, PRIMARY KEY (Username, PlaylistName, PlaylistPos));");
-session.execute("CREATE INDEX PlaylistPos ON UserDetails.UserPlaylists (PlaylistPos);");
-session.execute("CREATE INDEX TrackTitle ON UserDetails.UserPlaylists (TrackTitle);");
+session.execute("CREATE INDEX IF NOT EXISTS PlaylistPos ON UserDetails.UserPlaylists (PlaylistPos);");
+session.execute("CREATE INDEX IF NOT EXISTS TrackTitle ON UserDetails.UserPlaylists (TrackTitle);");
 session.close();
 }
 }
