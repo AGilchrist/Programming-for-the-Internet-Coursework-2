b330a7057991b448dd0eb56cf47efaab9928a371
Alistair <Alistair@10.97.194.154>
2014-03-21 15:40:27 +0000

    bumped the replication factor for keyspace up from 1

--- Coursework2/src/com/coursework2/alistair/models/Playlists.java
+++ Coursework2/src/com/coursework2/alistair/models/Playlists.java
@@ -19,7 +19,7 @@ public void CreateDatabase() {
 	
 Session session = cluster.connect();
 session.execute("CREATE KEYSPACE IF NOT EXISTS UserDetails WITH replication "
-         + "= {'class':'SimpleStrategy', 'replication_factor':1};");
+         + "= {'class':'SimpleStrategy', 'replication_factor':3};");
 session.execute("CREATE TABLE IF NOT EXISTS UserDetails.Users (UserName text, Password text, PRIMARY KEY (UserName));");
 session.execute("CREATE TABLE IF NOT EXISTS UserDetails.UserPlaylists (Username text, PlaylistName text, PlaylistPos int, TrackTitle text, Artist text, Album text, Trackid text, PRIMARY KEY (Username, PlaylistName, PlaylistPos));");
 session.execute("CREATE INDEX IF NOT EXISTS PlaylistPos ON UserDetails.UserPlaylists (PlaylistPos);");
